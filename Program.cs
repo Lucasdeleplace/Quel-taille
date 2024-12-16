5fb69ecc3ba23787b25b5975f247ba1d4dcf42d1
LucasDeleplace <Lucasdele59@gmail.com>
2024-12-16 15:40:40 +0100

    add(): Exercice finis

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿Console.WriteLine("--- Quelle taille dois-je prendre ? ---\n");
+
+Console.Write("Entrez votre taille (en cm) : ");
+int taille = int.Parse(Console.ReadLine());
+
+Console.Write("Entrez votre poids (en kg) : ");
+int poids = int.Parse(Console.ReadLine());
+
+if ((taille >= 145 && taille <= 162) && (poids >= 43 && poids <= 65) || taille <= 163 && (poids >= 43 && poids <= 59) || taille <= 166 && (poids >= 43 && poids <= 53) || taille <= 169 && (poids >= 43 && poids <= 47))
+{
+    Console.WriteLine("Prennez la taille 1");
+}
+else if ((taille >= 160 && taille < 163) && (poids >= 66 && poids <= 71) || taille <= 166 && (poids >= 60 && poids <= 71) || taille <= 169 && (poids >= 48 && poids <= 71) || taille <= 172 && (poids >= 48 && poids <= 65) || taille <= 175 && (poids >= 48 && poids <= 59) || taille <= 178 && (poids >= 48 && poids <= 53))
+{
+    Console.WriteLine("Prennez la taille 2");
+} 
+else if ((taille >= 163 && taille < 166) && (poids >= 72 && poids <= 77) || taille <= 169 && (poids >= 72 && poids <= 77) || taille <= 172 && (taille >= 66 && poids <= 77) || taille <= 175 && (poids >= 60 && poids <= 77) || taille <= 178 && (poids >= 54 && poids <= 77) || taille <= 183 && (poids >= 54 && poids <= 77))
+{
+    Console.WriteLine("Prennez la taille 3");
+}
+else
+{
+    Console.WriteLine("Aucune taille ne vous correspond");
+}
--- obj/Debug/net8.0/Quel-taille.AssemblyInfo.cs
+++ obj/Debug/net8.0/Quel-taille.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quel-taille")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afe4d6ca8364c7d6052d96f8c2ba892d33ce7ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quel-taille")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quel-taille")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
